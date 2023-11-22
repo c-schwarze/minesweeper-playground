423e48cfb76c10a7bf5f1ac8d3b063dd579c0438
Caleb Schwarze <ces55739@bethel.edu>
2023-11-22 13:46:15 -0600

    add in better READMEs

---
+++ python311/README.md
@@ -0,0 +1,1 @@
+The goal of this python repo is to build a BE that handles the board management. This will be used for a different FE.
--- python311/app.py
+++ python311/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    test='this is the test'
-    return test # render_template('hello.html', **locals())
+@app.route('/build-board/<width>/<height>/<mines>', methods=['GET'])
+def build_board(width, height, mines):
+    test='this is the test' + width + height + mines
+
+    # we will return the board as JSON
+    return jsonify([["M", "M", "M",], ["M", "M", "M",], ["M", "M", "M",],])
+
