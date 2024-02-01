4adc5432ac8875d56faad7f1d426c7220d0e9835
Caleb Schwarze <ces55739@bethel.edu>
2024-02-01 10:16:57 -0600

    create basic board and props

---
+++ vuejs/src/components/Board.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Square from './Square.vue'
+
+const props = defineProps<{
+  rows: Number,
+  cols: Number,
+  mines: Number,
+}>()
+
+// TODO - move this to a method, probably
+const boardData = ref([...Array(props.rows)].map(() => [...Array(props.cols)].map(() => 'x')));
+const minesRemaining = ref(0)
+</script>
+
+<template>
+  <div>
+    <p>Mines remaining: {{ props.mines}}</p>
+    <div class="board">
+      <div class="row" v-for="row in boardData">
+        <div class="square" v-for="square in row">
+          <!-- <Square /> -->
+          {{ square }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .board {
+    border: 2px black solid;
+    display: inline-block;
+    text-align: center;
+  }
+  .row {
+    display: flex;
+  }
+
+  .square {
+    width: 30px;
+    height: 30px;
+    border: 1px black solid;
+  }
+</style>
---
+++ vuejs/src/components/Square.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// defineProps<{
+  
+// }>()
+</script>
+
+<template>
+  <div class="square">
+    X
+  </div>
+</template>
+
+<style scoped>
+
+</style>
